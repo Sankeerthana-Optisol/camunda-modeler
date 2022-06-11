51db3d0e72eac585e0778d6db45b62053f90a1e1
Sankeerthana-Optisol <sankeerthana.s@optisolbusiness.com>
2022-06-12 01:48:07 +0530

    commit -1 camunda modeler in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as BpmnJS from 'bpmn-js/dist/bpmn-modeler.production.min.js';
+import minimapModule from 'diagram-js-minimap';
+import BpmnColorPickerModule from 'bpmn-js-color-picker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'camunda-modeler';
+
+  private bpmnJS: BpmnJS
+
+  private diagram: any
+
+  @ViewChild('ref', { static: true }) private el: ElementRef;
+  constructor(){
+    this.bpmnJS = new BpmnJS({
+      additionalModules: [
+        minimapModule,
+        BpmnColorPickerModule
+      ]
+    })
+  }
+
+  ngOnInit(): void{
+    fetch('../assets/template.bpmn')
+      .then(response => response.text()) 
+      .then(xml => {
+          this.bpmnJS.importXML(xml)
+      });
+  }
+
+  ngAfterContentInit(): void {
+    this.bpmnJS.attachTo(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.bpmnJS.destroy();
+  }
+
+  zoomIn() {
+    this.bpmnJS.get('zoomScroll').stepZoom(1);
+  }
+
+  zoomOut() {
+    this.bpmnJS.get('zoomScroll').stepZoom(-1);
+  }
 }
